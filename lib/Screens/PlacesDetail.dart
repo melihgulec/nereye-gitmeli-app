292ed26033f1f757684607e4f10808ea90736df6
unknown <melih.gulec14@gmail.com>
2021-05-06 21:47:29 +0300

    Yer detayı sayfası tasarımı güncellendi

--- lib/Screens/PlacesDetail.dart
+++ lib/Screens/PlacesDetail.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:nereye_gitmeli_app/Classes/Places/Places.dart';
 import 'package:nereye_gitmeli_app/Classes/Sehir/Sehir.dart';
@@ -29,69 +31,84 @@ class _PlacesDetailState extends State<PlacesDetail> {
         title: Text('Yer: ${widget.yerData.adi}/${widget.sehirData.adi}'),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    child: PlacesCard(
-                      sehirData: widget.sehirData,
-                      placeData: widget.yerData,
-                      haveText: false,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image(
+                  fit: BoxFit.fitWidth,
+                  image: AssetImage('assets/images/${widget.sehirData.type == 1? 'Yurtici' : 'Yurtdisi'}/Mekan/${widget.sehirData.adi}/${widget.yerData.fotograf}.jpg'),
+                  height: 150,
+                  width: double.infinity,
+                ),
+                ClipRRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Container(
+                            child: PlacesCard(
+                              sehirData: widget.sehirData,
+                              placeData: widget.yerData,
+                              haveText: false,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: Text(
+                              '${widget.yerData.adi}\n\n${widget.sehirData.adi}',
+                              style: TextStyle(fontSize: 20),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Expanded(
-                    child: Container(
-                      child: Text(
-                        '${widget.yerData.adi}\n\n${widget.sehirData.adi}',
-                        style: TextStyle(fontSize: 20),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                ],
+                ),
+              ]
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Card(
+              color: Theme.of(context).primaryColor,
+              child: ListTile(
+                leading: Icon(
+                  Icons.map,
+                  color: Colors.white,
+                ),
+                trailing: Icon(
+                  Icons.chevron_right,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Haritada göster',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onTap: () {
+                  _launchMapsUrl(widget.yerData.adi);
+                },
               ),
-              SizedBox(
-                height: 15,
-              ),
-              Card(
-                color: Theme.of(context).primaryColor,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.map,
-                    color: Colors.white,
-                  ),
-                  trailing: Icon(
-                    Icons.chevron_right,
-                    color: Colors.white,
-                  ),
-                  title: Text(
-                    'Haritada göster',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onTap: () {
-                    _launchMapsUrl(widget.yerData.adi);
-                  },
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            ContainerWithTitle(
+              title: '${widget.yerData.adi} Hakkında',
+              titleSize: 15,
+              widget: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: double.infinity,
+                  child: Text('${widget.yerData.aciklama}'),
                 ),
               ),
-              SizedBox(
-                height: 15,
-              ),
-              ContainerWithTitle(
-                title: '${widget.yerData.adi} Hakkında',
-                titleSize: 15,
-                widget: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: double.infinity,
-                    child: Text('${widget.yerData.aciklama}'),
-                  ),
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
